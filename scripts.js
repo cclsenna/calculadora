75f20444eba058ef493436c8a3d57e7536601313
Caio Senna <caiosenna93@gmail.com>
2022-01-31 23:20:41 -0300

    buttons event listener

---
+++ scripts.js
@@ -0,0 +1,47 @@
+//coloca o evetlisetener nos numeros
+$('.container__item--num').on('click',(e)=>{
+    const valor=e.target.innerText;
+    console.log(`Pegou o valor ${e.target.innerText}`);
+    
+}
+);
+
+
+
+function sum(a,b){
+    return a+b;
+}
+
+function subtract(a,b){   
+    return a-b;
+
+}
+
+function multiply(a,b){
+    return a*b;
+
+}
+
+function divide(a,b){
+    return a/b;
+
+}
+
+
+
+
+
+//essa função irá manipular o array da fila
+function controller(){
+
+
+}
+
+function addLista(){
+
+}
+
+//criar uma classe  para os obvjetos na quqla ele terão o valor od  número e 
+
+
+
