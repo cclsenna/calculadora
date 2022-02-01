33aadc301623da97b7f50787a4c270f084fbdaa5
Caio Senna <caiosenna93@gmail.com>
2022-02-01 16:07:35 -0300

    building functions for operations

--- scripts.js
+++ scripts.js
@@ -1,31 +1,51 @@
+const operacao=[];
+
 //coloca o evetlisetener nos numeros
 $('.container__item--num').on('click',(e)=>{
     const valor=e.target.innerText;
-    console.log(`Pegou o valor ${e.target.innerText}`);
+    const id=e.target.id;  
+
+
+    //console.log(`Pegou o valor ${e.target.innerText}`);
+    //console.log('o id desse cara é : '+id);
+    addLista(e.target.innerText);
+
     
 }
 );
 
 
 
-function sum(a,b){
-    return a+b;
+const operacoes= {
+    sum(a,b){
+        return a+b;
+    },
+    
+    subtract(a,b){   
+        return a-b;
+    
+    },
+    
+    multiply(a,b){
+        return a*b;
+    
+    },
+    
+    divide(a,b){
+        return a/b;
+    
+    },
+    clear(){
+        operacao=[];
+    },
+    allClear(){
+        operacao=[];
+
+    }
+    
+
 }
 
-function subtract(a,b){   
-    return a-b;
-
-}
-
-function multiply(a,b){
-    return a*b;
-
-}
-
-function divide(a,b){
-    return a/b;
-
-}
 
 
 
@@ -37,8 +57,10 @@ function controller(){
 
 }
 
-function addLista(){
-
+function addLista(value){
+    operacao.push(value);
+    console.log(`adicionando o ${value} na fila de execução`);
+    console.log(operacao);
 }
 
 //criar uma classe  para os obvjetos na quqla ele terão o valor od  número e 
